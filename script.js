986ebc1a0e477190d7854539cf1c11d84a0141c4
Robert Mudron <robino1498@gmail.com>
2025-02-12 15:34:08 +0100

    Add functions to get input from user and random input from computer

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello world!");
+function getComputerChoice() {
+    // get random number between 0 and 2 including
+    let randInt = Math.floor(Math.random() * 3);
+
+    if (randInt === 0) return "rock";
+    else if (randInt === 1) return "paper";
+    else return "scissors";
+}
+
+function getHumanChoice() {
+    let input = prompt("Enter of of rock, paper or scissors: ").toLowerCase();
+    console.log(input);
+    
+    if (!(input === 'rock' || input === 'paper' || input === 'scissors')) {
+        console.log("That's an invalid choice!");
+        return;
+    }
+
+    return input;
+
+
+}
+
+getHumanChoice();
