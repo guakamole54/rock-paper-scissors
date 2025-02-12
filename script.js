0c684248155bb076f3b586b007f7fe34d6604ed7
Robert Mudron <robino1498@gmail.com>
2025-02-12 15:51:55 +0100

    Implement 1 playing round logic and take count.

--- script.js
+++ script.js
@@ -21,4 +21,26 @@ function getHumanChoice() {
 
 }
 
-getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice === computerChoice) {
+        {console.log(`It's a tie ${humanChoice} ties with ${computerChoice}`)}
+    } else if ((humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'paper' && computerChoice === 'rock') || (humanChoice === 'scissors' && computerChoice === 'paper')) {
+
+        {console.log(`You win: ${humanChoice} beats ${computerChoice}!`); humanScore ++;}
+
+    }else {
+        {console.log(`You lose: ${computerChoice} beats ${humanChoice}!`); computerScore ++;}
+    }
+
+    console.log(`The score is YOU: ${humanScore} COMPUTER: ${computerScore}`);
+
+
+
+}
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+playRound(getHumanChoice(), getComputerChoice());
